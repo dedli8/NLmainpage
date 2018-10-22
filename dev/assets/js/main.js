1e51b7d7a0b8318033161912b84efcf91a6f560c
Dedli <dedli888@gmail.com>
2018-10-22 12:02:58 +0300

    adaptive perfectpixel secondcatalig is done

--- dev/assets/js/main.js
+++ dev/assets/js/main.js
@@ -8,18 +8,30 @@ $(function () {
         arrows: false,
         dotsClass: 'banner-dots'
     });
-    function dotsLeftPosition () {
-        if ($(window).width() > 1336){
-        const dotsLeftPosition = ($(window).width()-1336)/2+"px";
-        console.log(dotsLeftPosition);
-        $('.banner-dots').css("left", dotsLeftPosition);
+
+    function dotsLeftPosition() {
+        if ($(window).width() > 1336) {
+            const dotsLeftPosition = ($(window).width() - 1336) / 2 + "px";
+            console.log(dotsLeftPosition);
+            $('.banner-dots').css("left", dotsLeftPosition);
         }
-        else{
+        else {
             $('.banner-dots').css("left", "2%");
         }
     }
+
     dotsLeftPosition();
     $(window).resize(function () {
         dotsLeftPosition();
+    });
+    $(".second-catalog .menu .menu-item").click(function (e) {
+        // $(".second-catalog .menu .menu-item-active .info-item-btn").css("display", "block");
+        $(".menu-item-active").removeClass("menu-item-active");
+        $(e.target).addClass("menu-item-active");
+        const clickedMenuItem = $(e.target).attr("data-attribute");
+        $(".second-catalog .info .info-item-wrap").removeClass("info-item-wrap-active");
+        const currentInfoItem = $(".second-catalog .info div#" + clickedMenuItem);
+        currentInfoItem.addClass("info-item-wrap-active");
+        // currentInfoItem.find(".info-item-btn").css("display", "none");
     })
 });
