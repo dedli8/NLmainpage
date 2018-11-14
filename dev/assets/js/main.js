0bcec2da4f67a8190bed7c6cf05e32384c3560b6
dedli <dedli888@gmail.com>
2018-11-14 15:48:42 +0200

    cities dropdown is done

--- dev/assets/js/main.js
+++ dev/assets/js/main.js
@@ -1,15 +1,56 @@
 $(function () {
-    //count left position of dots depending of devise width
-    function dotsLeftPosition(dotsClass, maxWidth) {
-        if ($(window).width() > maxWidth) {
-            const dotsLeftPosition = ($(window).width() - maxWidth) / 2 + "px";
-            console.log(dotsLeftPosition);
-            dotsClass.css("left", dotsLeftPosition);
-        }
-        else {
-            dotsClass.css("left", "2%");
-        }
+    //hide it when clicking anywhere else except the popup and the trigger
+    function hideClickAnywhere (trigger, hideTarget) {
+        $(document).on('click touch', function (event) {
+            if (!$(event.target).parents().addBack().is('#'+trigger)) {
+                $('#'+hideTarget).hide();
+            }
+        });
     }
+
+    // cities change
+    $('#selected-city').click(function (e) {
+        $('#cities-dropdown').toggle();
+        hideClickAnywhere('selected-city', 'cities-dropdown');
+    });
+    $('#cities-dropdown .cities-dropdown-item').click(function (e) {
+        $('#cities-dropdown .cities-dropdown-item').removeClass('cities-dropdown-item-active');
+        $(e.target).parent().addClass('cities-dropdown-item-active');
+        console.log($(this));
+        // console.log($(e.target).parent().find('address'));
+        let selectedCity = $(this).find('.city').text();
+        let selectedAddress = $(this).find('.address').text();
+        $('#selected-city').find('.city').text(selectedCity);
+        $('#selected-city').find('.address').text(selectedAddress);
+        $('#cities-dropdown').hide();
+    });
+
+
+    //dropdown catalog menu
+    $('#dropdown-catalog-toggle').mouseover(function () {
+        $('#dropdown-catalog').css({"display": "block"});
+    });
+    $('#dropdown-catalog').mouseleave(function () {
+        $('#dropdown-catalog').css("display", "none");
+        $('.dropdown-inner-menu').removeClass('dropdown-catalog-active-ib');
+        $('.dropdown-catalog-wrap').css({"width": "25%", 'display': 'block'});
+        $('.dropdown-menu-wrap').css({'width': '100%', 'display': 'block'});
+        $('.grid').masonry('destroy');
+    });
+    $('.dropdown-menu-item a').mouseover(function (e) {
+        $('.dropdown-inner-menu').removeClass('dropdown-catalog-active-ib');
+        const currentDdItem = $(e.target).attr("data-attribute");
+        $('div#' + currentDdItem).addClass("dropdown-catalog-active-ib");
+        $('.dropdown-catalog-wrap').css({"width": "100%"});
+        $('.dropdown-menu-wrap').css({'width': '25%', 'display': 'inline-block'});
+        $('.dropdown-connector').css({'width': "18%"});
+        $('.inner-menu').masonry({
+            // options
+            itemSelector: '.inner-menu-item',
+            columnWidth: 197,
+            transitionDuration: '1.5s'
+        });
+    });
 
     //languge panel
     $('.language-panel-item').click(function (e) {
@@ -20,13 +61,21 @@ $(function () {
     //enter form
     $('#enter-btn').click(function (e) {
         $('#enter-popup').toggle();
+        hideClickAnywhere('enter-btn', 'enter-popup');
     });
-    //hide it when clicking anywhere else except the popup and the trigger
-    $(document).on('click touch', function (event) {
-        if (!$(event.target).parents().addBack().is('#enter-btn')) {
-            $('#enter-popup').hide();
+
+
+    //count left position of dots depending of devise width on main banner
+    function dotsLeftPosition(dotsClass, maxWidth) {
+        if ($(window).width() > maxWidth) {
+            const dotsLeftPosition = ($(window).width() - maxWidth) / 2 + "px";
+            console.log(dotsLeftPosition);
+            dotsClass.css("left", dotsLeftPosition);
         }
-    });
+        else {
+            dotsClass.css("left", "2%");
+        }
+    }
 
 
     //init main banner slider
@@ -52,26 +101,10 @@ $(function () {
         variableWidth: true,
         arrows: false,
         swipe: false,
-        // responsive: [
-        //     {
-        //         breakpoint: 940,
-        //         settings: {
-        //             slidesToShow: 3,
-        //             slidesToScroll: 3,
-        //         }
-        //     },
-        //     {
-        //         breakpoint: 768,
-        //         settings: {
-        //             slidesToShow: 2,
-        //             slidesToScroll: 2
-        //         }
-        //     }
-        // ]
     });
 
 
-    // tabs
+    // second catalog slider
     function tabs(menuItem, catalogSliderItemActive) {
         menuItem.on('click', function (e) {
             $('.catalog-slider-item .info-item-wrap').off('mouseenter');
@@ -83,34 +116,12 @@ $(function () {
             $('.second-catalog-slider').slick('slickGoTo', sliderIndex);
             addInnerSlider();
 e.stopPropagation();
-
-            // console.log($(e.target).parent().hasClass('slick-active'));
-            // console.log((menuItem.length-1)/2);
-            // if (dataSlickIndex==0){
-            //     console.log("Yes");
-            // }
-            // while(dataSlickIndex!="0"){
-            // $('.second-catalog-slider').slick("slickNext");
-            // $('.second-catalog-slider').slick("slickNext");
-            // let dataSlickIndex = $(e.target).parent(".catalog-slider-item").attr('tabindex');
-            //     $('.second-catalog-slick-next').click( function () {
-            //         dataSlickIndex = $(e.target).parent(".catalog-slider-item").attr('tabindex');
-            //         console.log(dataSlickIndex);
-            //     });
-            // const dataIndex = $(e.target).parent(".catalog-slider-item").attr('data-index');
-            // console.log(dataIndex);
-            // console.log($('.second-catalog-slider').slick('slickCurrentSlide'));
-            // dataSlickIndex = $(e.target).parent(".catalog-slider-item").attr('tabindex');
-            // console.log(dataSlickIndex);
-            // console.log(dataSlickIndex);
-            // }
         });
     }
-
     tabs($(".second-catalog .menu-item"), "catalog-slider-item-active");
     addInnerSlider();
 
-    //add inner slider on hover
+    //add inner slider on hover to second catalog
     function addInnerSlider() {
         $('.catalog-slider-item-active .info-item-wrap').mouseenter(function (e) {
             console.log($(this));
@@ -198,30 +209,5 @@ e.stopPropagation();
         arrows: true
     });
 
-//dropdown catalog menu
-    $('#dropdown-catalog-toggle').mouseover(function () {
-        $('#dropdown-catalog').css({"display": "block"});
-    });
-    $('#dropdown-catalog').mouseleave(function () {
-        $('#dropdown-catalog').css("display", "none");
-        $('.dropdown-inner-menu').removeClass('dropdown-catalog-active-ib');
-        $('.dropdown-catalog-wrap').css({"width": "25%", 'display': 'block'});
-        $('.dropdown-menu-wrap').css({'width': '100%', 'display': 'block'});
-        $('.grid').masonry('destroy');
-    });
-    $('.dropdown-menu-item a').mouseover(function (e) {
-        $('.dropdown-inner-menu').removeClass('dropdown-catalog-active-ib');
-        const currentDdItem = $(e.target).attr("data-attribute");
-        $('div#' + currentDdItem).addClass("dropdown-catalog-active-ib");
-        $('.dropdown-catalog-wrap').css({"width": "100%"});
-        $('.dropdown-menu-wrap').css({'width': '25%', 'display': 'inline-block'});
-        $('.dropdown-connector').css({'width': "18%"});
-        $('.inner-menu').masonry({
-            // options
-            itemSelector: '.inner-menu-item',
-            columnWidth: 197,
-            transitionDuration: '0.5s'
-        });
-    });
 
 });
